208df7b8ad3a65c585461512f3e590199347303e
BrainStone <yannick@tedworld.de>
2021-08-26 23:24:05 +0200

    Started reimplementing the interface in a more sane way

---
+++ src/main/cpp/TMInterface.cpp
@@ -0,0 +1,27 @@
+#include "TMInterface.h"
+
+#include <sstream>
+
+namespace TMInterface {
+
+Interface::Interface(const std::string& name) : buffer(name, BUF_SIZE), registered(false) {}
+
+Interface::Interface(size_t index) : Interface(getNameFromIndex(index)) {}
+
+Interface::~Interface() {}
+
+std::string Interface::getNameFromIndex(size_t index) {
+	std::ostringstream stream;
+
+	if (index >= MAX_SERVERS) {
+		stream << "Index " << index << " out of range 0 to " << (MAX_SERVERS - 1);
+
+		throw std::out_of_range(stream.str());
+	}
+
+	stream << "TMInterface" << index;
+
+	return stream.str();
+}
+
+}  // namespace TMInterface
---
+++ src/main/cpp/TMInterface.inc.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "TMInterface.h"
+
+#ifdef TMInterface_Proper_Included
+
+namespace TMInterface {}
+
+#endif
